6b0f75acbc0801c49a1cb50861e6ae59b97101ca
rpoetrap <rzkputrapamungkas65@gmail.com>
2020-08-04 08:41:11 +0700

    Change stats data

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,26 +20,18 @@ const IndexPage: FunctionComponent<Props> = (props: Props) => {
 	const { } = props;
 
 	const [pagination, setPagination] = useState<Pagination>({ pageIndex: 1, itemsPerPage: 10, currentItemCount: 1, totalItems: 1, totalPages: 1, sorts: '' });
-	const data = [
-		{
-			name: 'Page A', uv: 590, pv: 800, amt: 1400, cnt: 490,
-		},
-		{
-			name: 'Page B', uv: 868, pv: 967, amt: 1506, cnt: 590,
-		},
-		{
-			name: 'Page C', uv: 1397, pv: 1098, amt: 989, cnt: 350,
-		},
-		{
-			name: 'Page D', uv: 1480, pv: 1200, amt: 1228, cnt: 480,
-		},
-		{
-			name: 'Page E', uv: 1520, pv: 1108, amt: 1100, cnt: 460,
-		},
-		{
-			name: 'Page F', uv: 1400, pv: 680, amt: 1700, cnt: 380,
-		},
-	];
+	const statsData: any[] = [];
+
+	for (const start = moment().startOf('isoWeek'); start.isBefore(moment().endOf('isoWeek')); start.add(1, 'day')) {
+		const masuk = start.isSameOrBefore(moment()) ? Math.round(Math.random() * 100) : 0;
+		statsData.push({
+			date: start.format('DD/MM/YYYY'),
+			masuk: masuk,
+			keluar: Math.round(Math.random() * masuk)
+		})
+	}
+
+	console.log(statsData);
 
 	const options = [
 		{
@@ -113,14 +105,14 @@ const IndexPage: FunctionComponent<Props> = (props: Props) => {
 						<CardBody>
 							<h5 className={styles['mb-3']}>Statistik Kendaraan</h5>
 							<ResponsiveContainer height={320} >
-								<AreaChart data={data} >
+								<AreaChart data={statsData} >
 									<CartesianGrid stroke="#f5f5f5" />
-									<XAxis dataKey="name" />
+									<XAxis dataKey="date" />
 									<YAxis />
 									<Tooltip />
 									<Legend />
-									<Area type="natural" dataKey="amt" fill="#007bff" stroke="#007bff" strokeWidth={0} dot={true} />
-									<Area type="natural" dataKey="pv" fill="red" stroke="red" strokeWidth={0} dot={true} />
+									<Area type="basis" dataKey="masuk" fill="#007bff" stroke="#007bff" strokeWidth={0} />
+									<Area type="basis" dataKey="keluar" fill="red" stroke="red" strokeWidth={0} />
 								</AreaChart>
 							</ResponsiveContainer>
 						</CardBody>
@@ -142,13 +134,13 @@ const IndexPage: FunctionComponent<Props> = (props: Props) => {
 								<div className="col-12 col-md-6 col-lg-12 col-xl-6 px-2 my-2">
 									<div className="card p-2 text-white bg-primary border-0" style={{ borderRadius: '0.5rem' }}>
 										<small>Kendaraan Masuk</small>
-										<h3 className="m-0 font-weight-normal">10.023</h3>
+										<h3 className="m-0 font-weight-normal">{statsData.map(item => item.masuk).reduce((a, b) => (a + b))}</h3>
 									</div>
 								</div>
 								<div className="col-12 col-md-6 col-lg-12 col-xl-6 px-2 my-2">
 									<div className="card p-2 text-white bg-danger border-0" style={{ borderRadius: '0.5rem' }}>
 										<small>Kendaraan Keluar</small>
-										<h3 className="m-0 font-weight-normal">10.023</h3>
+										<h3 className="m-0 font-weight-normal">{statsData.map(item => item.keluar).reduce((a, b) => (a + b))}</h3>
 									</div>
 								</div>
 							</div>
@@ -156,7 +148,7 @@ const IndexPage: FunctionComponent<Props> = (props: Props) => {
 								<div className="col-12 px-2 my-2">
 									<div className="card p-2 text-white bg-success border-0" style={{ borderRadius: '0.5rem' }}>
 										<small>Penghasilan</small>
-										<h3 className="m-0 font-weight-normal">Rp 10.023</h3>
+										<h3 className="m-0 font-weight-normal">Rp {statsData.map(item => item.keluar).reduce((a, b) => (a + b)) * 1000}</h3>
 									</div>
 								</div>
 							</div>
