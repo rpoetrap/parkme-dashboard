f6e67427680ce222809ba5aaacdc6c2de93625d7
rpoetrap <rzkputrapamungkas65@gmail.com>
2020-08-05 00:55:13 +0700

    Passing env

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 const withPlugins = require("next-compose-plugins");
 const withCSS = require("@zeit/next-css");
 const withSASS = require("@zeit/next-sass");
+require('dotenv').config({ path: '.env' });
 
+const nextConfig = {
+	env: {
+		API_HOST: process.env.API_HOST
+	}
+}
 module.exports = withPlugins([
   [withCSS],
   [withSASS, {
     cssModules: true
   }]
-]);
+], nextConfig);
