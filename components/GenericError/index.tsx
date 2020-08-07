66c4a0e0f31449496e3c46017d67906318965aa1
rpoetrap <rzkputrapamungkas65@gmail.com>
2020-08-07 17:54:48 +0700

    Add error title

--- components/GenericError/index.tsx
+++ components/GenericError/index.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from "react";
 import Link from "next/link";
+import Head from "next/head";
 
 interface Props {
 
@@ -8,13 +9,20 @@ interface Props {
 const GenericError: FunctionComponent<Props> = (props: Props) => {
 	const { } = props;
 	return (
-		<div className="vw-100 vh-100 d-flex flex-column align-items-center justify-content-center">
-			<h1>404</h1>
-			<div className="mb-2">Halaman tidak ditemukan</div>
-			<Link href={'/'}>
-				<a className="btn btn-primary">Kembali</a>
-			</Link>
-		</div>
+		<>
+			<Head>
+				<title>Error | ParkMe</title>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			</Head>
+			<div className="vw-100 vh-100 d-flex flex-column align-items-center justify-content-center">
+				<h1>404</h1>
+				<div className="mb-2">Halaman tidak ditemukan</div>
+				<Link href={'/'}>
+					<a className="btn btn-primary">Kembali</a>
+				</Link>
+			</div>
+		</>
 	)
 }
 
